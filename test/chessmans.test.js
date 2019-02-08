ba8b2de582f03d2b24272ab7d6b5cd4a99a119d4
macbook <macbook@macbookdeMacBook-Pro.local>
2019-02-08 22:50:17 +0800

    renaming

--- test/chessmans.test.js
+++ test/chessmans.test.js
@@ -1,11 +1,11 @@
 import {
   toFen,
   fromFen
-} from './fen'
+} from './chessmans'
 
 import chessmans from './chessmans.test.json'
 
-describe('fen', () => {
+describe('chessmans', () => {
   const FEN = 'rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR'
 
   it('toFen', () => {
