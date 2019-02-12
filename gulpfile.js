dd06200c0e5f9049188f470e979d9d334783e078
macbook <macbook@macbookdeMacBook-Pro.local>
2019-02-12 12:25:52 +0800

    renaming

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp')
+var babel = require('gulp-babel')
 var clean = require('gulp-clean')
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
@@ -15,10 +16,14 @@ var folder = 'node_modules/xqbase.com/xqwlight'
 
 var libs = files.map(file => `${folder}/${file}`)
 
-var sources = libs.concat([
+var sources_xqwlight = libs.concat([
 	'src/board.js',
-	'src/index.js'
-	// 'src/main.js'
+	'src/xqwlight.js'
+])
+
+var sources_eve = libs.concat([
+	'src/board.js',
+	'src/eve.js'
 ])
 
 var sources_ai = libs.concat([
@@ -30,22 +35,29 @@ gulp.task('clean', () => {
 		.pipe(clean())
 })
 
-gulp.task('concat', () => {
-	return gulp.src(sources)
+gulp.task('concat-xqwlight', () => {
+	return gulp.src(sources_xqwlight)
 		.pipe(concat('xqwlight.js'))
-		// .pipe(uglify())
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'))
+})
+
+gulp.task('concat-eve', () => {
+	return gulp.src(sources_eve)
+		.pipe(concat('eve.js'))
 		.pipe(gulp.dest('dist'))
 })
 
 gulp.task('concat-ai', () => {
 	return gulp.src(sources_ai)
 		.pipe(concat('ai.js'))
-		// .pipe(uglify())
+		.pipe(babel())
+		.pipe(uglify())
 		.pipe(gulp.dest('dist'))
 })
 
 gulp.task('copy', (cb) => {
-	var assets = ['src/style.css', 'src/index.htm', 'node_modules/xqbase.com/background.gif']
+	var assets = ['src/style.css', 'src/xqwlight.htm', 'src/eve.htm', 'node_modules/xqbase.com/background.gif']
 	var resources = ['sounds', 'images']
 
 	let count = 0
@@ -66,5 +78,5 @@ gulp.task('copy', (cb) => {
 })
 
 gulp.task('default', (cb) => {
-	runSequence('clean', 'concat', 'concat-ai', 'copy', cb)
+	runSequence('clean', 'concat-xqwlight', 'concat-eve', 'concat-ai', 'copy', cb)
 })
